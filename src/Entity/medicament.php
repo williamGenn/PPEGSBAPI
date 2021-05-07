6a5d4cbbda94b00d62462e2c7c7e22cbbdb7cb61
OrioPrisco <adri.ringard@gmail.com>
2021-05-07 10:26:20 +0200

    annotation

--- src/Entity/medicament.php
+++ src/Entity/medicament.php
@@ -2,6 +2,7 @@
 
 namespace App\Entity;
 
+use ApiPlatform\Core\Annotation\ApiResource;
 use App\Repository\medicamentRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
